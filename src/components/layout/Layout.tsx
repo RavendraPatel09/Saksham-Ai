f6fa4358c960d8161caa59925224d6d56981b9d8
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-15 23:52:58 +0530

    feat: enhance Layout component with MoreDrawer and improved state management

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { BottomNav } from './BottomNav';
 import { SakhiAI } from '../ui-custom/SakhiAI';
 import { AccessibilityPanel } from '../ui-custom/AccessibilityPanel';
+import { MoreDrawer } from './MoreDrawer';
 
 export const Layout = () => {
+  const [isMoreOpen, setIsMoreOpen] = useState(false);
+
   return (
     <div className="flex flex-col min-h-screen pb-16 md:pb-0 relative">
-      <Navbar />
+      <Navbar onOpenMore={() => setIsMoreOpen(true)} />
       <main className="flex-1">
         <Outlet />
       </main>
-      <BottomNav />
+      <BottomNav onOpenMore={() => setIsMoreOpen(true)} />
+      <SakhiAI />
+      <AccessibilityPanel />
+      <MoreDrawer isOpen={isMoreOpen} onClose={() => setIsMoreOpen(false)} />
+    </div>
+  );
+};      <BottomNav />
       <SakhiAI />
       <AccessibilityPanel />
     </div>
