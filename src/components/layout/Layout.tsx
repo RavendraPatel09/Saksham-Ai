8a187d85e9f011d3ca2cbf304836d5ea10d0f0d6
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-15 18:31:16 +0530

    feat: add SakhiAI component to Layout for enhanced user interaction

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,16 +3,18 @@ import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
 import { BottomNav } from './BottomNav';
 import { AccessibilitySwitcher } from '../ui-custom/AccessibilitySwitcher';
+import { SakhiAI } from '../ui-custom/SakhiAI';
 
 export const Layout = () => {
   return (
-    <div className="flex min-h-screen flex-col bg-background text-foreground transition-colors duration-300 pb-16 md:pb-0">
+    <div className="flex min-h-screen flex-col bg-background text-foreground transition-colors duration-300 pb-16 md:pb-0 relative">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
       <BottomNav />
       <AccessibilitySwitcher />
+      <SakhiAI />
     </div>
   );
 };
