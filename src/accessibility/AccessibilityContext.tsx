8a09151a55ca2d8f0d40682fadc5f7a6ab1a4419
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-15 18:54:20 +0530

    fix: change import to type for AccessibilityPreferences and AccessibilityProfile in AccessibilityContext

--- src/accessibility/AccessibilityContext.tsx
+++ src/accessibility/AccessibilityContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { AccessibilityPreferences, AccessibilityProfile } from './AccessibilitySettings';
+import type { AccessibilityPreferences, AccessibilityProfile } from './AccessibilitySettings';
 
 export interface AccessibilityContextType {
   prefs: AccessibilityPreferences;
