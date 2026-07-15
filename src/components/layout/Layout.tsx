8483fdeaa7795f7f66e006eb56a73662515c8843
RavendraPatel09 <patelsamerth9@gmail.com>
2026-07-15 18:41:24 +0530

    feat: replace Footer with BottomNav in Layout component for improved structure

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navbar } from './Navbar';
-import { Footer } from './Footer';
+import { BottomNav } from './BottomNav';
 import { SakhiAI } from '../ui-custom/SakhiAI';
 import { AccessibilityPanel } from '../ui-custom/AccessibilityPanel';
 
 export const Layout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen pb-16 md:pb-0 relative">
       <Navbar />
       <main className="flex-1">
         <Outlet />
       </main>
-      <Footer />
+      <BottomNav />
       <SakhiAI />
       <AccessibilityPanel />
     </div>
